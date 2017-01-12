e47287f6afe2d3eea0ecee67b0c35b793c8c56f9
Jeffrey Berthiaume <jeffrey@Jeffreys-MacBook-Pro.local>
2017-01-11 22:41:20 -0600

    seach for FAQ almost complete

--- helpstream/helpstream/Screens/FAQViewController.swift
+++ helpstream/helpstream/Screens/FAQViewController.swift
@@ -11,10 +11,17 @@ import Foundation
 class FAQViewController: UIViewController {
     var faqData: [FAQCategory] = []
     var currentData: [FAQCategory] = []
+    
+    var flatSearchData: [FAQCategory] = []
+    var searchResultData: [FAQCategory] = []
+    
     var faqIndex: [Int] = []
     var previousPageTitle: String = "Help"
     var webView: UIWebView = UIWebView()
     
+    var shouldShowSearchResults: Bool = false
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
     
@@ -67,6 +74,7 @@ class FAQViewController: UIViewController {
         webView.isHidden = true
         
         loadData()
+        flatSearchData = flattenSearchData(categories: faqData)
         
         currentData = faqData
         
@@ -84,8 +92,62 @@ class FAQViewController: UIViewController {
         
         faqData = [faq1, faq2]
         
-        print (faqData)
+        //print (faqData)
     }
+    
+    func flattenSearchData(categories: [FAQCategory]) -> [FAQCategory] {
+        // go through all levels and flatten them
+        var arr: [FAQCategory] = []
+        for i in 0..<categories.count {
+            let item = categories[i]
+            if (item.details != nil) {
+                arr.append(item)
+            } else if let subCategories = item.subCategories {
+                arr = arr + flattenSearchData(categories: subCategories)
+            }
+        }
+        return arr
+    }
+}
+
+extension FAQViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let searchString = searchBar.text
+        
+        searchResultData = flatSearchData.filter({ (item) -> Bool in
+            let str: NSString = item.title.appending(" ".appending(item.details!)) as NSString
+            return (str.range(of: searchString!, options: .caseInsensitive).location != NSNotFound)
+        })
+        
+        if searchResultData.count == 0 {
+            shouldShowSearchResults = false
+        } else {
+            shouldShowSearchResults = true
+        }
+        tableView.reloadData()
+    }
+    
 }
 
 extension FAQViewController: UITableViewDelegate {
@@ -117,12 +179,23 @@ extension FAQViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currentData.count
+        if shouldShowSearchResults == true {
+            return searchResultData.count
+        } else {
+            return currentData.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "FAQCategoryCell")
-        let currentItem: FAQCategory = currentData[indexPath.row]
+        
+        var currentItem: FAQCategory
+        if shouldShowSearchResults == true {
+            currentItem = searchResultData[indexPath.row]
+        } else {
+            currentItem = currentData[indexPath.row]
+        }
+        
         cell.textLabel?.text = currentItem.title
         cell.accessoryType = .disclosureIndicator
         
